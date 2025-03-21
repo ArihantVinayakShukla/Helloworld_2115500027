e461b843d4941e5a9e927ccb2c3858eac1b497e0
ArihantVinayakShukla <shuklaarihant0@gmail.com>
2025-03-21 15:07:15 +0530

    [Arihant] Add. completed the use case Uc3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,25 @@ export class AppComponent {
   imgUrl = "../assets/bridgeLabz_Logo.png";
   url = "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string = "";
 
   onClick($event : Event){
     console.log("Save button is clicked", $event);
     window.open(this.url, "_blank");
   }
+
+  onInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.userName = inputElement.value; 
+  
+    console.log("Change Event Occurred!", this.userName);
+  
+    const nameRegex = /^[A-Z]{1}[a-zA-Z\s]{2,}$/;
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
+  }
+  
 }
